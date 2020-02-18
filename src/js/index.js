91b153344191fa550170fbe7ce45a507bc498798
Daniel Feles <dfeles@gmail.com>
2020-02-18 20:04:58 +0100

    manymany

--- src/js/index.js
+++ src/js/index.js
@@ -6,15 +6,27 @@ $('#alert').click(() => {
   alert('jQuery works!');
 });
 
-
-$(window).scroll(function () {
+$(window).scroll(function() {
   var a = $(window).scrollTop();
-  if(a<50) {
-    $('.navbar').addClass("hidden");
+  if (a < 200) {
+    $('.raptor-logo-top').addClass('hidden');
   } else {
-    $('.navbar').removeClass("hidden");
+    $('.raptor-logo-top').removeClass('hidden');
   }
   console.log(a);
+});
+
+$('.iphoneX.video').click(function(e) {
+  var target = $(e.target);
+  var video = target.find('video').get(0);
+  var playButton = target.find('.playButton');
+  if (video.paused) {
+    video.play();
+    playButton.addClass('hidden');
+  } else {
+    video.pause();
+    playButton.removeClass('hidden');
+  }
 });
 
 // Your jQuery code
