e04d5bbb8c960fc44132c10f41d2359ff22c74e0
Szyly <szilard.bozoki@gmail.com>
2020-02-21 15:27:47 +0100

    lazy loading for videos and images added

--- src/js/index.js
+++ src/js/index.js
@@ -13,7 +13,6 @@ $(window).scroll(function() {
   } else {
     $('.raptor-logo-top').removeClass('hidden');
   }
-  console.log(a);
 });
 
 $('.iphoneX.video').click(function(e) {
@@ -29,4 +28,50 @@ $('.iphoneX.video').click(function(e) {
   }
 });
 
-// Your jQuery code
+$(document).ready(function() {
+  let lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
+  let lazyVideos = [].slice.call(document.querySelectorAll('video.lazy'));
+  let active = false;
+  const lazyLoad = function() {
+    if (active === false) {
+      active = true;
+      setTimeout(function() {
+        lazyImages.forEach(function(lazyImage) {
+          lazyImage.src = lazyImage.dataset.src;
+
+          lazyImage.classList.remove('lazy');
+
+          lazyImages = lazyImages.filter(function(image) {
+            return image !== lazyImage;
+          });
+
+          if (lazyImages.length === 0 && lazyVideos.length === 0) {
+            document.removeEventListener('scroll', lazyLoad);
+            window.removeEventListener('resize', lazyLoad);
+            window.removeEventListener('orientationchange', lazyLoad);
+          }
+        });
+        lazyVideos.forEach(function(lazyVideo) {
+          lazyVideo.poster = lazyVideo.dataset.poster;
+
+          lazyVideo.classList.remove('lazy');
+
+          lazyVideos = lazyVideos.filter(function(Video) {
+            return Video !== lazyVideo;
+          });
+
+          if (lazyVideos.length === 0 && lazyVideos.length === 0) {
+            document.removeEventListener('scroll', lazyLoad);
+            window.removeEventListener('resize', lazyLoad);
+            window.removeEventListener('orientationchange', lazyLoad);
+          }
+        });
+        active = false;
+      }, 200);
+    }
+  };
+
+  document.addEventListener('scroll', lazyLoad);
+  window.addEventListener('resize', lazyLoad);
+  window.addEventListener('orientationchange', lazyLoad);
+});
